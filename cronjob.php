b8053fe03a829ac4380cb0fdc447a3038733efb1
Jaime Munizaga <jaime.munizaga.l@gmail.com>
2016-11-28 17:47:13 -0300

    pruebas para com serial -php controlador

--- cronjob.php
+++ cronjob.php
@@ -6,4 +6,4 @@
  * and open the template in the editor.
  */
 
-echo "controlador_raspberry_Test";
+
--- programador.php
+++ programador.php
@@ -1,26 +1,70 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
+define(ARDUINO_PORT, "/dev/ttyACM0");
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+//$ stty -F /dev/ttyACM0 cs8 9600 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
+//sudo chmod 777 /dev/ttyACM0
 
-
-$server_address="http://192.168.1.85/aplicacion-web-v2";
+$server_address = "http://192.168.1.85/aplicacion-web-v2";
 $request_page = "/request.php";
+$listener_page = "/listener.php";
 
-$url=$server_address.$request_page;
+$urlRequest = $server_address . $request_page;
+$urlListener = $server_address . $listener_page;
 
-$response = file_get_contents($url);
+$respuesta = file_get_contents($urlRequest);
+$tarea = json_decode($respuesta, true);
 
-echo "data:";
-echo $response;
-echo "<br>";
-//var_dump($response);
-$object = json_decode($response, true);
+if (isset($tarea)) {
 
+    switch ($tarea["comando"]) {
+        case "acc":
+            actuador($tarea["pin"], $tarea["valor"]);
+            break;
+        case "med":
+            sensor($tarea["pin"]);
+            break;
+        case "inf":
+            break;
+        default:
+            break;
+    }
+}
 
+function actuador($pin, $valor) {
+    $orden = "acc," . $pin . "," . $valor;
+    $serial = fopen(ARDUINO_PORT, "w+");
+    if (!$serial) {
+        echo "error";
+        die();
+    }
+    sleep(2);
+    fwrite($serial, $orden);
+    sleep(2);
+    fclose($serial);
+}
+
+function sensor($pin) {
+    $orden = "med," . $pin . "," . "0";
+    global $urlListener;
+    $response = "";
+    $serial = fopen(ARDUINO_PORT, "w+");
+    if (!$serial) {
+        echo "error";
+        die();
+    }
+    sleep(2);
+    fwrite($serial, $orden);
+    sleep(2);
+    while (!feof($serial)) {
+        $response = fgets($serial);
+        break;
+    }
+    //$data = json_decode($response,true);
+    $url = $urlListener . "?data=" . $response;
+    file_get_contents($url);
+    fclose($serial);
+}
